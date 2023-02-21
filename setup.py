d394efd93c2292b3207f50376d3e2bf0fa2a000f
MartinHasal <m.hasal@email.cz>
2023-02-21 17:46:33 +0100

    chore: initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Program to classify a between good and bad quality of medical imaging data by deep leaning. It is written in Python/OpenCV/TensorFlow. It is applied to retinal images, but implementations alows the usage to any image data.',
+    author='Martin Hasal',
+    license='',
+)
