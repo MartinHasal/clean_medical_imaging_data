7dca2528d1f8403730989a8660961774ce316451
MartinHasal <m.hasal@email.cz>
2023-03-20 13:48:13 +0100

    add ROC curve and confusion matrix

--- utils/plots.py
+++ utils/plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pylab as plt
 import numpy as np
 import os, shutil, tempfile, subprocess
+from math import sqrt
 
 # numpy and matplotlib defaults
 np.set_printoptions(threshold=15, linewidth=80)
@@ -12,9 +13,9 @@ def training_plot(metrics, history, filename):
         ax[idx].plot(history.history[metric], ls='dashed')
         ax[idx].set_xlabel("Epochs")
         ax[idx].set_ylabel(metric)
-        ax[idx].plot(history.history['val_' + metric]);
+        ax[idx].plot(history.history['val_' + metric])
         ax[idx].legend([metric, 'val_' + metric])
-    
+
     on_cloud = filename.startswith('gs://')
     if on_cloud:
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -25,7 +26,7 @@ def training_plot(metrics, history, filename):
     else:
         plt.savefig(filename)
 
-        
+
 def batch_to_numpy_images_and_labels(data):
     images, labels = data
     # print('Zde ',labels  )
@@ -34,12 +35,15 @@ def batch_to_numpy_images_and_labels(data):
         return images, labels
     numpy_images = images.numpy()
     numpy_labels = labels.numpy()
-    if numpy_labels.dtype == object: # binary string in this case, these are image ID strings
+    # binary string in this case, these are image ID strings
+    if numpy_labels.dtype == object:
         numpy_labels = [None for _ in enumerate(numpy_images)]
-    # If no labels, only image IDs, return None for labels (this is the case for test data)
+    # If no labels, only image IDs, return None for labels
+    # (this is the case for test data)
     return numpy_images, numpy_labels
 
-def title_from_label_and_target(label, correct_label):
+
+def title_from_label_and_target(label, correct_label, CLASSES):
     # print(label, correct_label)
     if correct_label is None:
         return CLASSES[label], True
@@ -56,9 +60,9 @@ def display_one_img(image, title, subplot, red=False, titlesize=16):
     if len(title) > 0:
         plt.title(title, fontsize=int(titlesize) if not red else int(titlesize/1.2), color='red' if red else 'black', fontdict={'verticalalignment':'center'}, pad=int(titlesize/1.5))
     return (subplot[0], subplot[1], subplot[2]+1)
-    
 
-def display_batch_of_images(databatch, predictions=None, precision=None):
+
+def display_batch_of_images(databatch, CLASSES=None, predictions=None, precision=None):
     """Function works with:
     display_batch_of_images(images)
     display_batch_of_images(images, predictions)
@@ -70,8 +74,9 @@ def display_batch_of_images(databatch, predictions=None, precision=None):
     if labels is None:
         labels = [None for _ in enumerate(images)]
 
-    # auto-squaring: this will drop data that does not fit into square or square-ish rectangle
-    rows = int(math.sqrt(len(images)))
+    # auto-squaring: this will drop data that does not fit into square
+    # or square-ish rectangle
+    rows = int(sqrt(len(images)))
     cols = len(images)//rows
 
     # size and spacing
@@ -83,22 +88,23 @@ def display_batch_of_images(databatch, predictions=None, precision=None):
     else:
         plt.figure(figsize=(FIGSIZE/rows*cols, FIGSIZE))
 
-
     # display
     for i, (image, label) in enumerate(zip(images[:rows*cols], labels[:rows*cols])):
         title = '' if label is None else CLASSES[label]
         correct = True
         if predictions is not None:
-            title, correct = title_from_label_and_target(predictions[i], label)
+            title, correct = title_from_label_and_target(predictions[i],
+                                                         label,
+                                                         CLASSES)
             # print(title)
         if precision is not None:
-            title = title +'-'+ str(np.round(precision[i],3))
+            title = title + '-' + str(np.round(precision[i], 3))
         # magic formula tested to work from 1x1 to 10x10 images
         dynamic_titlesize = FIGSIZE*SPACING/max(rows, cols)*40+3
         subplot = display_one_img(
             image, title, subplot, not correct, titlesize=dynamic_titlesize)
 
-    #layout
+    # layout
     plt.tight_layout()
     if label is None and predictions is None:
         plt.subplots_adjust(wspace=0, hspace=0)
@@ -107,27 +113,24 @@ def display_batch_of_images(databatch, predictions=None, precision=None):
     plt.show()
 
 
-def display_confusion_matrix(cmat, score, precision, recall):
-    plt.figure(figsize=(15,15))
-    ax = plt.gca()
-    ax.matshow(cmat, cmap='Reds')
-    ax.set_xticks(range(len(CLASSES)))
-    ax.set_xticklabels(CLASSES, fontdict={'fontsize': 7})
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="left", rotation_mode="anchor")
-    ax.set_yticks(range(len(CLASSES)))
-    ax.set_yticklabels(CLASSES, fontdict={'fontsize': 7})
-    plt.setp(ax.get_yticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    titlestring = ""
-    if score is not None:
-        titlestring += 'f1 = {:.3f} '.format(score)
-    if precision is not None:
-        titlestring += '\nprecision = {:.3f} '.format(precision)
-    if recall is not None:
-        titlestring += '\nrecall = {:.3f} '.format(recall)
-    if len(titlestring) > 0:
-        ax.text(101, 1, titlestring, fontdict={'fontsize': 18, 'horizontalalignment':'right', 'verticalalignment':'top', 'color':'#804040'})
-    plt.show()
-    
+def plot_confusion_matrix(cm, target_names, title='Confusion Matrix', cmap=plt.cm.Greens):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+    plt.tight_layout()
+
+    width, height = cm.shape
+
+    for x in range(width):
+        for y in range(height):
+            plt.annotate(str(cm[x][y]), xy=(y, x),
+                         horizontalalignment = 'center',
+                         verticalalignment = 'center')
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
 
 def display_training_curves(training, validation, title, subplot, zoom_pcent=None, ylim=None):
     # zoom_pcent: X autoscales y axis for the last X% of data points
@@ -152,5 +155,15 @@ def display_training_curves(training, validation, title, subplot, zoom_pcent=Non
     ax.set_xlabel('epoch')
     ax.legend(['train', 'valid.'])
 
-    
-        
+
+def ROC_plot(fpr, tpr, AUC):
+    plt.figure(figsize=(10, 10))
+    plt.plot(fpr, tpr, lw=1, label='ROC curve (area = %0.2f)' % AUC)
+    plt.plot([0, 1], [0, 1], color='lime', linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
